f4bce5712b2660524b546d854c5e6e9eb4c09a9e
safi-wasif <safiwasif100@gmail.com>
2025-01-25 02:58:26 +0530

    connect to mongo atlass

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js"
+import express from "express"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+//const app =express()
+
+
+// ( async ()=> {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         application.on("error" ,  (error)=>{console.log("err: ", error)});
+//         throw error
+
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`app is listening on port: ${process.env.PORT}`)
+//         })
+//     }
+
+    
+//     catch(error){
+//         console.error("ERR");
+//         throw error
+//     }
+// })()
